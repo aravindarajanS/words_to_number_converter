d00660f532eb62a71ca4f47764b372239795ee35
aravindarajanS <87989427+aravindarajanS@users.noreply.github.com>
2024-04-22 17:12:42 +0200

    initial comit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Number to word conversion
+
+This code repo is a demo to convert the numbers to french words.
+## Features
+
+- Input can be a single number or list of numbers seperated by space
+- The limit of the conversion is 99999
+- for the moment the output is not save to a file as it is just for demo.
+
+
+## Installation
+
+- No special libraries are used , hence there is no requirement.txt
+- Used coverage library to check the script flow for different input
+
+```bash
+pip install coverage
+```
+
+## Run
+
+```bash
+python main.py 0 1 5 10 11 15 20 21 30 35 50 51 68 70 75 99 100 101 105 111 123 168 171 175 199 200 201 555 999 1000 1001 1111 1199 1234 1999 2000 2001 2020 2021 2345 9999 10000 11111 12345 123456 654321 999999
+
+```
+
+```bash
+python main.py 500
+```
+
+for coverage please run the following commands
+
+```bash
+coverage run src/numbers_to_word.py
+
+coverage html
+```
+
+## Data
+
+There is no seperate data needed for this demo.
+
+## File Structure
+
+- src/main.py -- main program that converts the number to words
+- src/unit_test.py -- run unit test on the main function
+- htmlcov/index.html -- script flow for differnt input.
---
+++ src/unit_test.py
@@ -0,0 +1,37 @@
+import unittest
+from numbers_to_word import number_to_french_words
+
+
+class TestFrenchWord(unittest.TestCase):
+
+    def test_single_digit_conversion(self):
+        self.assertEqual(number_to_french_words(13), "treize")
+
+    def test_tens_and_units_conversion(self):
+        self.assertEqual(number_to_french_words(68), "soixante-huit")
+
+    def test_hundreds_conversion(self):
+        self.assertEqual(number_to_french_words(789), "sept-cent-quatre-vingt-neuf")
+
+    def test_large_number_conversion(self):
+        self.assertEqual(number_to_french_words(99999), "quatre-vingt-dix-neuf-mille-neuf-cent-quatre-vingt-dix-neuf")
+
+    def test_hundred_conversion(self):
+        self.assertEqual(number_to_french_words(100), "cent")
+
+    def test_thousands_conversion(self):
+        self.assertEqual(number_to_french_words(5005), "cinq-mille-cinq")
+        
+
+    def test_negative_number_error(self):
+        with self.assertRaises(ValueError):
+            number_to_french_words(-10)
+
+    def test_decimal_number_error(self):
+        with self.assertRaises(ValueError):
+            number_to_french_words(3.14)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
